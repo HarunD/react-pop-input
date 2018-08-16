58b8c03c24d170f918ed753f8ae15ddb037fbe8f
Harun Dulic <harundulic@gmail.com>
2018-08-16 10:55:25 +0200

    Initial commit

---
+++ src/PopInput.tsx
@@ -0,0 +1,91 @@
+/*
+    React Pop Input
+*/
+import React, {FormEvent} from 'react';
+
+import './style.css';
+
+interface PopInputProps {
+    inputClassName?: string;
+    inputPosition?: string;
+    rootClassName?: string;
+    saveOnEnter : boolean;
+    style?: object;
+    value : string;
+}
+
+interface PopInputState {
+    inputValue : string;
+    isPopVisible : boolean;
+    value : string;
+}
+
+interface InputOptions {
+    className?: string;
+    saveOnEnter : boolean;
+    value : string;
+}
+
+export default class PopInput extends React.Component < PopInputProps,
+PopInputState > {
+    public static defaultProps : Partial < PopInputProps > = {
+        inputClassName: '',
+        inputPosition: 'bottom',
+        rootClassName: '',
+        saveOnEnter: true,
+        value: ''
+    }
+
+    constructor(props : PopInputProps) {
+        super(props);
+        this.state = {
+            inputValue: props.value,
+            isPopVisible: false,
+            value: props.value
+        };
+    }
+
+    componentDidUpdate(prevProps : any, prevState : any) {}
+
+    _toggleInput = () : void => {
+        this.setState({
+            isPopVisible: !this.state.isPopVisible
+        });
+    }
+
+    _handleChange = (e : FormEvent < HTMLInputElement >) : void => {
+        this.setState({inputValue: e.currentTarget.value});
+    }
+
+    _handleSave = (value : string) : void => {
+        this.setState({value: value, isPopVisible: false});
+    }
+
+    _renderInput = (opt : InputOptions) : JSX.Element => {
+        return <input
+            className={`PopInput__Input ${opt.className}`}
+            type="text"
+            value={opt.value}
+            onChange={this._handleChange}
+            onKeyPress={(e : any) => {
+            if (opt.saveOnEnter && e.key === 'Enter') {
+                this._handleSave(opt.value);
+            }
+        }}/>;
+    }
+
+    render() {
+        let {rootClassName, inputClassName, inputPosition, style, saveOnEnter} = this.props;
+
+        return (
+            <span
+                className={`PopInput ${inputPosition} ${rootClassName
+                ? rootClassName
+                : ''}`}
+                style={style}>
+                <span onClick={this._toggleInput}>{this.state.value}</span>
+                {this.state.isPopVisible && this._renderInput({value: this.state.inputValue, className: inputClassName, saveOnEnter})}
+            </span>
+        );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/PopInput.tsx',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'index.js',
+        libraryTarget: 'commonjs2'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(tsx|js)?$/,
+                include: path.resolve(__dirname, 'src'),
+                exclude: /(node_modules|build)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                'react', 'es2015'
+                            ],
+                            plugins: ['transform-class-properties']
+                        }
+                    }, {
+                        loader: 'ts-loader'
+                    }
+                ]
+            }, {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    externals: {
+        'react': 'commonjs react'
+    }
+};
